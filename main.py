7b73e69fc1b77c23f1ffe9c651f37e256d32cd53
qortjddbs(notebook) <beak980513@naver.com>
2025-10-17 18:49:59 +0900

    게임 실행에 필요한 함수들 생성

--- main.py
+++ main.py
@@ -3,13 +3,31 @@ from pico2d import *
 
 running = True
 
-
-
 open_canvas()
 
-# game loop
-while running:
 
+def reset_world():
+    pass
+
+
+reset_world()
+# game loop
+def handle_events():
+    pass
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    pass
+
+
+while running:
+    handle_events()
+    update_world()
+    render_world()
     delay(0.01)
 # finalization code
 close_canvas()
