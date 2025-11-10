11c85e756a4a8478e1a42ed26c1633269b906264
qortjddbs(notebook) <beak980513@naver.com>
2025-11-10 23:59:09 +0900

    커밋을 안했네; 시작화면 만들고 게임 시작 버튼 누르면 게임모드로 들어가게끔 변경

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pico2d import open_canvas, delay, close_canvas
 import game_framework
-
-import play_mode as start_mode
+import title_mode
+import play_mode
 
 open_canvas(550, 400)
-game_framework.run(start_mode)
+game_framework.run(title_mode)
 close_canvas()
 
---
+++ title_mode.py
@@ -0,0 +1,62 @@
+from pico2d import *
+import game_framework
+import play_mode
+import select_mode
+
+font = None
+background_image = None
+logo_image = None
+start_button_image = None
+method_button_image = None
+
+
+def init():
+    global background_image, logo_image, start_button_image, method_button_image, font
+    background_image = load_image('./Background/시작배경.png')
+    logo_image = load_image('./Background/로고.png')
+    start_button_image = load_image('./Background/시작버튼.png')
+    method_button_image = load_image('./Background/방법버튼.png')
+    font = load_font('ENCR10B.TTF', 20)
+
+
+def finish():
+    global background_image, logo_image, start_button_image, method_button_image, font
+    del background_image
+    del logo_image
+    del start_button_image
+    del method_button_image
+    del font
+
+
+def update():
+    pass
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            # 마우스 클릭 좌표 계산
+            mouse_x = event.x
+            mouse_y = 400 - 1 - event.y  # 캔버스 높이가 400
+            print(f"마우스 클릭: ({mouse_x}, {mouse_y})")
+
+            # 시작 버튼 클릭 영역 체크 (475, 300 중심)
+            if 425 <= mouse_x <= 525 and 285 <= mouse_y <= 315:
+                print("시작 버튼 클릭!")
+                game_framework.change_mode(play_mode)
+
+
+def draw():
+    clear_canvas()
+    background_image.draw(277, 200)
+    logo_image.draw(200, 200)
+    start_button_image.draw(475, 300)
+    # font.draw(460, 300, 'Start', (0, 0, 0))
+    method_button_image.draw(475, 250)
+    # font.draw(467, 253, 'Key', (0, 0, 0))
+    update_canvas()
+
