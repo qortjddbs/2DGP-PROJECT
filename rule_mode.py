4af2a28d6dc97ecf816bea353769a272bd5290bb
qortjddbs(notebook) <beak980513@naver.com>
2025-11-30 00:19:40 +0900

    커서 바꾸기

--- rule_mode.py
+++ rule_mode.py
@@ -6,25 +6,32 @@ rule_image = None
 show_back_red = False
 back_red_image = None
 back_image = None
+cursor_image = None
+mouse_x, mouse_y = 0, 0
 
 def init():
-    global rule_image, back_red_image, back_image
+    global rule_image, back_red_image, back_image, show_back_red, cursor_image
+    cursor_image = load_image('./Background/cursor.png')
+    show_back_red = False
     rule_image = load_image('./Background/Key.png')
     back_red_image = load_image('./Background/back(red).png')
     back_image = load_image('./Background/back.png')
+    hide_cursor()
 
 def finish():
-    global rule_image, back_red_image, back_image
+    global rule_image, back_red_image, back_image, cursor_image
+    del cursor_image
     del back_red_image
     del rule_image
     del back_image
+    show_cursor()
 
 
 def update():
     pass
 
 def handle_events():
-    global show_back_red
+    global show_back_red, mouse_x, mouse_y
     event_list = get_events()
     for event in event_list:
         if event.type == SDL_QUIT:
@@ -35,12 +42,12 @@ def handle_events():
             # 마우스 클릭 좌표 계산
             mouse_x = event.x
             mouse_y = 400 - 1 - event.y  # 캔버스 높이가 400
-
-            if 470 <= mouse_x <= 530 and 20 <= mouse_y <= 40:
+            if 485 <= mouse_x <= 545 and 5 <= mouse_y <= 25:
                 show_back_red = True
             else:
                 show_back_red = False
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            print(mouse_x, mouse_y)
             if show_back_red:
                 game_framework.change_mode(title_mode)
 
@@ -52,4 +59,5 @@ def draw():
         back_red_image.draw(500, 30)
     else:
         back_image.draw(500, 30)
+    cursor_image.draw(mouse_x, mouse_y)
     update_canvas()
--- title_mode.py
+++ title_mode.py
@@ -1,6 +1,5 @@
 from pico2d import *
 import game_framework
-import play_mode
 import rule_mode
 import setting_mode
 
@@ -9,48 +8,54 @@ background_image = None
 logo_image = None
 start_button_image = None
 method_button_image = None
-
+cursor_image = None
+mouse_x, mouse_y = 0, 0
 
 def init():
-    global background_image, logo_image, start_button_image, method_button_image, font
+    global background_image, logo_image, start_button_image, method_button_image, font, cursor_image
     background_image = load_image('./Background/시작배경.png')
     logo_image = load_image('./Background/로고.png')
     start_button_image = load_image('./Background/시작버튼.png')
     method_button_image = load_image('./Background/방법버튼.png')
     font = load_font('ENCR10B.TTF', 20)
-
+    cursor_image = load_image('./Background/cursor.png')
+    hide_cursor()
 
 def finish():
-    global background_image, logo_image, start_button_image, method_button_image, font
+    global background_image, logo_image, start_button_image, method_button_image, font, cursor_image
     del background_image
     del logo_image
     del start_button_image
     del method_button_image
     del font
+    del cursor_image
 
+    show_cursor()
 
 def update():
     pass
 
 def handle_events():
+    global mouse_x, mouse_y
     event_list = get_events()
     for event in event_list:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif event.type == SDL_MOUSEMOTION:
+            # 마우스 클릭 좌표 계산
+            mouse_x = event.x
+            mouse_y = 400 - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
             # 마우스 클릭 좌표 계산
             mouse_x = event.x
             mouse_y = 400 - 1 - event.y  # 캔버스 높이가 400
-            print(f"마우스 클릭: ({mouse_x}, {mouse_y})")
 
             # 시작 버튼 클릭 영역 체크 (475, 300 중심)
-            if 425 <= mouse_x <= 525 and 210 <= mouse_y <= 240:
-                print("시작 버튼 클릭!")
+            if 435 <= mouse_x <= 540 and 190 <= mouse_y <= 225:
                 game_framework.change_mode(setting_mode)
-            elif 425 <= mouse_x <= 525 and 160 <= mouse_y <= 190:
-                print("방법 버튼 클릭!")
+            elif 440 <= mouse_x <= 540 and 140 <= mouse_y <= 175:
                 game_framework.change_mode(rule_mode)
 
 
@@ -60,5 +65,6 @@ def draw():
     logo_image.draw(200, 200)
     start_button_image.draw(475, 225)
     method_button_image.draw(475, 175)
+    cursor_image.draw(mouse_x, mouse_y)
     update_canvas()
 
