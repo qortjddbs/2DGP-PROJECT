fa540c74b4edc20687d1a5c9afe1922edeac1912
qortjddbs(notebook) <beak980513@naver.com>
2025-12-01 22:30:45 +0900

    궁극기 사용 시 mp감소 로직 개발

--- murloc.py
+++ murloc.py
@@ -104,7 +104,7 @@ class Jump:
             self.murloc.state_machine.cur_state = self.murloc.SKILL
             self.murloc.SKILL.enter(('AIR_SKILL_HELD', None))
             return  # JUMP.do()를 즉시 종료
-        if ult_pressed:
+        if ult_pressed and self.murloc.mp >= 40:
             self.murloc.state_machine.cur_state = self.murloc.ULT
             self.murloc.ULT.enter(('AIR_ULT_HELD', None))
             return  # JUMP.do()를 즉시 종료
